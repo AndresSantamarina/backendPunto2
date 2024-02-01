3711227509dba176951b66f9990e9b98260f2cef
Andres Santamarina <andresesantamarina@gmail.com>
2024-02-01 18:46:54 -0300

    agregue el maquetado del componente footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
-  
-
   return (
     <>
-      
+      <Container className="my-4">
+        <h1 className="text-center">Bienvenido</h1>
+      </Container>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
