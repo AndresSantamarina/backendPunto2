180c46076d866b9bf1caa60b1e52415796476ed8
Andres Santamarina <andresesantamarina@gmail.com>
2024-02-01 18:51:38 -0300

    cree el componente FormularioTarea y lo importe en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Footer from "./components/Footer";
+import FormularioTarea from "./components/FormularioTarea";
 
 function App() {
   return (
     <>
       <Container className="my-4">
         <h1 className="text-center">Bienvenido</h1>
+        <FormularioTarea />
       </Container>
       <Footer />
     </>
